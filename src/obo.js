4ab25335964a21deda2a7f84ebe996e35c248dc3
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-12-20 01:06:57 -0500

    logs all non-empty lines

--- src/obo.js
+++ src/obo.js
@@ -1,5 +1,10 @@
 'use strict'
 
+const through = require('through2')
+const es = require('event-stream')
+const fs = require('fs')
+const util = require('./util')
+
 /**
  * Parse OBO files. Emits a `term` object stream.
  * @module bionode-obo
@@ -13,3 +18,52 @@
 exports.parse = function () {
   return 'foo'
 }
+
+
+
+let cm = util.charCodeMap('[]{}:\n')
+let j = 0
+
+let curr = 0
+let next
+
+let stream = fs.createReadStream('SyRO.obo')
+  .pipe(through(function (chunk, enc, cb) {
+    for (let i=0; i < chunk.length; i++) {
+      if (chunk[i] === cm['\n']) {
+        j++
+      }
+      let activeColon = false
+      // if (chunk[i] == cm[':'] && !activeColon) {
+      //   next = util.getNextNewline(curr, chunk)
+      //   let mini = chunk.slice(curr, next)
+      //   let key = chunk.slice(curr,i).toString()
+      //   let val = chunk.slice(i+1, next).toString()
+      //   curr = next
+      //
+      //   activeColon = true
+      //   console.log(JSON.stringify({key, val}))
+      //   // this.emit('error', null)
+      // }
+      if (chunk[i] == cm['\n']) {
+        next = util.getNextNewline(curr, chunk)
+        let line = chunk.slice(curr, next)
+        curr = next+1
+        if (!(line.length === 0))
+          console.log('line:', line.toString())
+      }
+    }
+
+    this.push(chunk)
+    cb()
+  }))
+  .pipe(es.through(function(data) {
+    console.log(data.toString().split('\n').length)
+    console.log(j)
+    this.emit('data', data)
+  }))
+  // .pipe(process.stdout)
+
+stream.on('newline', function(data) {
+  console.log(data)
+})
