7c3fcff0b5b545ae8630830f1264d2355b057e48
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-02-09 11:22:06 -0500

    emits stringified terms, not differentiating from header

--- src/obo.js
+++ src/obo.js
@@ -1,56 +1,49 @@
 'use strict'
 
-const EventEmitter = require('events')
-const util = require('util')
-
-const through = require('through2')
-const es = require('event-stream')
-
+const _ = require('lodash')
 const highland = require('highland')
-const _ = require('./util')
-const CE = require('./emitter')
 
 /**
- * Parse OBO files. Emits a `term` object stream.
+ * Parse OBO files. Emits a [Term] object stream.
  * @module bionode-obo
  */
 
+
+/**
+ * Parse one [Term] at once.
+ *
+ * NOTE Although this parser is a stream, this function requires one term to be
+ * stored in memory at once. How else to create an Object stream? One
+ * could append a string until it is JSON.parse()''able, but that still requires
+ * storing the object in memory at once, on the other side. Better to provide
+ * an Object stream as well as an ndjson stream.
+ */
+const termParser = (term) => {
+  return term
+    .split('\n')
+    // Filter out empty lines
+    .filter(l => l.length !== 0)
+    // Reduce array into object of key:val
+    .reduce( (prev, curr) => { 
+      const sep = curr.indexOf(':')
+      const key = curr.substring(0, sep)
+      const val = _.trim(curr.substring(sep+1))
+      
+      prev[key] = val
+
+      return prev
+    }, {} )
+}
 
 /**
  * Parse OBO 1.2 file
  * @return {stream} the readable stream of an OBO file from fs or www
  */
 exports.parse = function (stream) {
-  let emitter = CE()
-  let ccm = _.charCodeMap('[]{}:\n')
-
-  let curr = 0
-  let next
-  let line
-
-  // Emit `line` events
-  stream.pipe(through( (chunk) => {
-    for (let i=0; i < chunk.length; i++) {
-      if (chunk[i] == ccm['\n']) {
-        next = _.getNextNewline(curr, chunk)
-        line = chunk.slice(curr, next)
-        curr = next + 1
-        if (!(line.length === 0)) {
-          emitter.writeline(line)
-        }
-      }
-    }
-  }))
-
-  emitter.on('line', (chunk) => {
-    console.log(chunk.toString())
-  })
-}
-
-exports.parseHL = function (stream) {
   highland(stream)
-    .splitBy('\n')
-    .filter(line => line.length !== 0)
-    .map(line => `${line}\n`)
+    .splitBy('[Term]')
+    .map(term => termParser(term))
+    .map(term => JSON.stringify(term, null, 2) + '\n')
     .pipe(process.stdout)
 }
+
