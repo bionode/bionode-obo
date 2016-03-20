32ba3e00ee9d848960c093c3d8761358bbcc05ad
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-03-20 02:30:51 -0400

    port back to good'ol through and BufferList, closes #24; also fixes #23, fixes #21

--- src/obo.js
+++ src/obo.js
@@ -1,152 +1,84 @@
 'use strict'
 
-const _ = require('lodash')
-const highland = require('highland')
-const EventEmitter2 = require('eventemitter2').EventEmitter2 || require('eventemitter2')
+const through = require('through2')
+const filter = require('through2-filter')
+const split = require('split')
+const BufferList = require('bl')
 
-/**
- * Parse OBO files from an input stream.
- * Emits a [Term] object stream or ndjson stream.
- * @module bionode-obo
- */
-
-console.log(require('eventemitter2').EventEmitter2)
-
-// Setting up event emitter
-const emitter = new EventEmitter2({
-  wildcard: false,
-  delimiter: '.',
-  newListener: true,
-  maxListeners: 10
-})
-
-// emitter.addListener('header', header => console.log('HEADER: ' + JSON.stringify(stanzaParser(header), null, 2)))
-
-// highland('stanza', emitter)
-//   .map(stanza => stanzaParser(stanza))
-//   .map(stanza => JSON.stringify(stanza, null, 2))
-//   .each(stanza => console.log('STANZA: ' + stanza))
+const charCode = (char) => new Buffer(char)[0]
 
 // Flags and buffers
 let header = true
-let headerComplete = false
-let headerStr = ''
+let stanzaBuffer = new BufferList()
 
-let term = false
-let termStr = ''
+const skipEmpties = filter(chunk => chunk.length !== 0)
 
-// Watch for `line` events from the emitter
-// TODO stop using events, as this stream "never ends"
-highland('line', emitter)
-  .each(line => {
-    // If we found a new stanza, swap term flag
-    if (line.match(/^\[[a-z]+\]/i)) {
-      // Found a stanza
-      header = false
-      term = !term
+const parseStanza = (stanzaBuffer) => stanzaBuffer
+  .toString()
+  .trim()
+  .split('\n')
+  .reduce((stanza, currentLine) => {
+    // add key:val to stanza
+    const keyValSeperatorIndex = currentLine.indexOf(':')
+    let key = currentLine.slice(0, keyValSeperatorIndex)
+    let val = currentLine.slice(keyValSeperatorIndex+1).trim()
+
+    if (currentLine[0] === '[') {
+      key = ''
+      val = val.replace(/^\[/, '').replace(/\]$/, '')
     }
 
-    // Emit header once
-    // TODO headerStr buffer can be termStr
-    if (header) {
-      headerStr += line + '\n'
-    } else if (!headerComplete) {
-      emitter.emit('header', headerStr)
-      headerComplete = true
+    // TODO know ahead of time which keys should be arrays
+    if (stanza[key]) {
+      if (stanza[key].constructor !== Array) {
+        stanza[key] = new Array(stanza[key])
+      }
+
+      stanza[key].push(val)
+    } else {
+      stanza[key] = val
     }
 
+    return stanza
+  }, {})
 
-    if (term) {
-      // If we are currently buffering a stanza, continue to do so
-      termStr += line + '\n'
-    } else if (!term && !header) {
-      // Term flag will have been swapped if we encountered a [Term], [Typedef], etc
-      // Then emit the currently completed stanza, and swap the flag back so we
-      // continue to buffer a new stanza
-      term = !term
-      emitter.emit('stanza', termStr)
-      termStr = line + '\n'
+const parse = (stream) => {
+
+  const transform = function (chunk, enc, next) {
+    // Line starts with [
+    if (chunk[0] === charCode('[')) {
+      if (header) {
+        this.emit('header', parseStanza(stanzaBuffer.slice()))
+        header = false
+      } else {
+        // Push what we have collected for the current set of key:value pairs
+        this.push(parseStanza(stanzaBuffer.slice()))
+      }
+
+      // and start again
+      stanzaBuffer = new BufferList()
     }
-  })
 
-// Emits a `line` event with each line passed in
-// TODO mitigate using events, event stream doesn't know when it ends
-const getLines = (stream) => {
+    stanzaBuffer.append(chunk).append('\n')
+    next()
+  }
+
+  const flush = function() {
+    this.push(parseStanza(stanzaBuffer.slice()))
+    this.push(null)
+  }
+
   return stream
-    .splitBy('\n')
-    .each(line => emitter.emit('line', line))
+    .pipe(split())
+    .pipe(skipEmpties)
+    .pipe(through.obj(transform, flush))
 }
 
-// map stanza text to an object representation
-const parseStanzas = (stream) => {
-  return stream.map(stanza => stanzaParser(stanza))
-}
-
-// map objects to stringified lines
-const ndjsonIfy = (stream) => {
-  return stream.map(obj => JSON.stringify(obj) + '\n')
-}
-
-// filter out [Term]s
-const termsFilter = (stream) => {
-  return stream.filter(obj => obj[''] === '[Term]')
-}
-
-/**
- * Parse OBO 1.2 file
- * @return {stream} the readable stream of an OBO file from fs or www
- */
-exports.parse = highland.pipeline(highland.through(getLines))
-
-/**
- * Produce [Term]s object stream
- * @param  {stream} stream stream from fs or www
- * @return {stream}        object stream
- */
-exports.terms = (stream) => {
-  highland(stream).through(getLines)
-
-  return highland('stanza', emitter)
-    .through(parseStanzas)
-    .through(termsFilter)
-}
-
-/**
- * Produce [Term]s ndjson stream
- * @param  {stream} stream strem from fs or www
- * @return {stream}        ndjson stream
- */
-exports.termsNdjson = (stream) => {
-  highland(stream).through(getLines)
-
-  return highland('stanza', emitter)
-    .through(parseStanzas)
-    .through(termsFilter)
-    .through(ndjsonIfy)
-}
-
-/**
- * Parse one [Term] at once.
- *
- * NOTE Although this parser is a stream, this function requires one term to be
- * stored in memory at once. How else to create an Object stream? One
- * could append a string until it is JSON.parse()''able, but that still requires
- * storing the object in memory at once, on the other side. Better to provide
- * an Object stream as well as an ndjson stream.
- */
-const stanzaParser = (stanza) => {
-  return stanza
-    .split('\n')
-    // Filter out empty lines
-    .filter(l => l.length !== 0)
-    // Reduce array into object of key:val
-    .reduce( (prev, curr) => {
-      const sep = curr.indexOf(':')
-      const key = curr.substring(0, sep)
-      const val = _.trim(curr.substring(sep+1))
-
-      prev[key] = val
-
-      return prev
-    }, {} )
+exports.parse = parse
+exports.parseNdjson = (stream) => {
+  return parse(stream)
+    .pipe(through.obj(function (obj, enc, next) {
+      this.push(JSON.stringify(obj))
+      next()
+    }))
 }
--- test/test.js
+++ test/test.js
@@ -3,15 +3,42 @@
 const fs = require('fs')
 const through = require('through2')
 const request = require('request')
+const hl = require('highland')
 
 const obo = require('../src/obo')
 
-const readStream = fs.createReadStream(__dirname + '/SyRO.obo')  
-const liveStream = request.get('http://purl.obolibrary.org/obo/go/go-basic.obo') 
+// const readStream = fs.createReadStream(__dirname + '/SyRO.obo')
+const readStream = fs.createReadStream(__dirname + '/ff-phase2-140729.obo')
+const liveStream = request.get('http://purl.obolibrary.org/obo/go/go-basic.obo')
 
 // readStream
 //   .pipe(obo.parse)
 //   .pipe(process.stdout)
+
+// readStream
+//   .pipe(through())
+//   .on('data', () => {
+//     console.log('got data')
+//   })
+//   .on('end', () => console.log('finished'))
+
+
+const receiveHeader = (data) => {
+  console.log(data)
+}
+
+// readStream
+//   .pipe(obo.parse)
+//   .pipe(process.stdout)
+
+hl(obo.parse(readStream).on('header', receiveHeader))
+  // .map(chunk => chunk.toString() + '=========')
+  .each(chunk => console.log(chunk))
+  .done(() => console.log('DONE'))
+
+// hl(readStream.pipe(through()))
+//   .each(chunk => console.log(chunk.toString()))
+//   .done(() => console.log('done'))
 
 // obo.terms(readStream)
 //   .pipe(through.obj( function(chunk, enc, cb) {
@@ -20,5 +47,11 @@ const liveStream = request.get('http://purl.obolibrary.org/obo/go/go-basic.obo')
 //     cb()
 //   } ))
 
-obo.termsNdjson(liveStream)
-  .pipe(process.stdout)
+// obo.termsNdjson(liveStream)
+//   .pipe(process.stdout)
+
+// obo.parse(liveStream)
+
+// liveStream
+//   .pipe(obo.parse)
+//   .pipe(process.stdout)
